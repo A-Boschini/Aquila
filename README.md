7c3616ebd0ff306314ca38a652ef30f047dfade0
Alberto <albertoboschinicr@gmail.com>
2021-07-06 20:50:22 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Wordpress Theme - Aquila-
+
+A Wordpress theme I am using to learn several things:
+1- Wordpress Theming
+2- Practicing Git and Github.
+3- Learning and improving additional programming skills.
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Main template file.
+*
+* @package Aquila
+*/
+ ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+     <head>
+         <meta charset="utf-8">
+         <title>Wordpress Theme</title>
+     </head>
+     <body>
+
+     </body>
+ </html>
